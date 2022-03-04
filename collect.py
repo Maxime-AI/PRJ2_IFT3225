fb4ed1a0e351ca671c62129c14d7f4288552273d
Kathrine Jalbert <kathrine.jalbert@hotmail.com>
2022-03-04 16:27:13 -0500

    implementation de la table de fait

---
+++ collect.py
@@ -0,0 +1,37 @@
+import requests
+import random
+
+# Ce script permet de récolter au moins 100 faits de la forme [start|relation|end] où
+# start et end sont des concepts de ConceptNet et relation est une relation particulière.
+#
+# EX : http://api.conceptnet.io/query?rel=/r/UsedFor&limit=1000
+
+relations = ["/r/RelatedTo", "/r/FormOf", "/r/IsA", "/r/PartOf", "/r/HasA", "/r/UsedFor", "/r/CapableOf", "/r/AtLocation", "/r/Causes", "/r/HasSubevent", "/r/HasFirstSubevent", "/r/HasLastSubevent", "/r/HasPrerequisite", "/r/HasProperty", "/r/MotivatedByGoal", "/r/ObstructedBy", "/r/Desires", "/r/CreatedBy", "/r/Synonym", "/r/Antonym", "/r/DistinctFrom", "/r/DerivedFrom", "/r/SymbolOf", "/r/DefinedAs", "/r/MannerOf", "/r/LocatedNear", "/r/HasContext", "/r/SimilarTo", "/r/EtymologicallyRelatedTo", "/r/EtymologicallyDerivedFrom", "/r/MadeOf", "/r/ReceivesAction"]
+
+objs = []
+while len(objs) < 100:
+	rel = relations[random.randint(0, len(relations) - 1)]
+	lg = '/c/en' if random.choice([True, False]) else '/c/fr'
+
+	obj = requests.get(f"https://api.conceptnet.io/query?rel={rel}&other={lg}&limit=1000").json()
+
+	if len(obj['edges']) < 1 :
+		continue
+
+	index = random.randint(0, len(obj['edges']) - 1)
+
+	# Get the relation
+	start = obj['edges'][index]['start']
+	relation = obj['edges'][index]['rel']
+	end = obj['edges'][index]['end']
+
+	# Check if language is french or english
+	if start['language'] == end['language'] :
+		info = {"start": {"@id": start['@id'], "label": start['label']}, "rel": {"@id": relation['@id'], "label": relation['label']}, "end": {"@id": end['@id'], "label": end['label']}}
+
+		objs.append(info)
+
+# Write JSON file.
+with open("result.json", 'w') as f:
+    f.write(f'{{"result": {objs}}}')
+
--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+$(document).ready(function(){
+  var nodeTable = $('<table>');
+  nodeTable.addClass("table center table-bordered table-striped table-responsive table-info")
 
-function main() {
-console.log('Hello, World!');
+  var nodesTable = ["Start", "Relation", "End"];
+  var node = $("<tr>");
+  $.each(nodesTable, function(index, elem){
+    node.append($("<th>").addClass("text-center").text(elem));
+  });
+  nodeTable.append(node);
 
-}
+  // Update Table with JSON data.
+  $.getJSON('result.json', function(data){
+    $.each(data.result, function(index, elem){
+
+      var n = $("<tr>");
+      $.each(elem, function(key, val){
+        n.append($("<td>").addClass(val["@id"]).text(val["label"]));
+      });
+      nodeTable.append(n);
+    });
+  });
+
+  $('#main').append(nodeTable);
+});
